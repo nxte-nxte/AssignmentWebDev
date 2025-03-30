93c095211f665b760e491b124ac87351fd163e2b
nxte-nxte <nathan.betton@dcmail.ca>
2025-03-30 03:12:22 -0400

    Looping through images

--- Assignment 4 - Javascript/Part 2/main.js
+++ Assignment 4 - Javascript/Part 2/main.js
@@ -8,15 +8,29 @@ const overlay = document.querySelector('.overlay');
 const image = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
 const altText = 
-[
+{
 'pic1.jpg': 'Eyes',
 'pic2.jpg': 'Rock',
 'pic3.jpg': 'Flower',
 'pic4.jpg': 'Tomb', 
 'pic5.jpg': 'Moth'  
-];
+};
 
 /* Looping through images */
+for (const img of image){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', img);
+    newImage.setAttribute('alt', altText[img]);
+    thumbBar.appendChild(newImage);
+
+    /* Adding an onclick event to each thumbnail */
+    newImage.addEventListener('click', () => {
+        displayedImage.setAttribute('src', img);
+        displayedImage.setAttribute('alt', altText[img]);
+    });
+
+    thumbBar.appendChild(newImage);
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
