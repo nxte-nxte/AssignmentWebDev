2c33e49c02994a871e0b378a9246c84b3a11a61a
nxte-nxte <nathan.betton@dcmail.ca>
2025-03-30 02:38:41 -0400

    Update main.js
    add image file name along with altText

--- Assignment 4 - Javascript/Part 2/main.js
+++ Assignment 4 - Javascript/Part 2/main.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const image = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
+const altText = 
+[
+'pic1.jpg': 'Eyes',
+'pic2.jpg': 'Rock',
+'pic3.jpg': 'Flower',
+'pic4.jpg': 'Tomb', 
+'pic5.jpg': 'Moth'  
+];
 
 /* Looping through images */
 
