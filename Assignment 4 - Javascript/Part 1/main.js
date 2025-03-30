178db299f2d307635674db87491d7c8887b5ba4d
nxte-nxte <nathan.betton@dcmail.ca>
2025-03-29 22:11:22 -0400

    Added conversion and Name modification

--- Assignment 4 - Javascript/Part 1/main.js
+++ Assignment 4 - Javascript/Part 1/main.js
@@ -36,18 +36,20 @@ function result() {
     newStory = newStory.replaceAll(':inserty:', yItem);
     newStory = newStory.replaceAll(':insertz:', zItem);
 
-
+    // Modifies the person's name in the story
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replaceAll('Bob', name);
   }
-
+// Modifies the weight and temperature based on the selected unit system
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300 * 0.0714286) + ' stone';
+    const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
