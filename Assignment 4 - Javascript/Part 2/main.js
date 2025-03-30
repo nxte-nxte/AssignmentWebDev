625d5ee2f96de5ba507c5dae3152ab18767879a1
nxte-nxte <nathan.betton@dcmail.ca>
2025-03-30 03:14:52 -0400

    Removed redundant code

--- Assignment 4 - Javascript/Part 2/main.js
+++ Assignment 4 - Javascript/Part 2/main.js
@@ -32,9 +32,4 @@ for (const img of image){
     thumbBar.appendChild(newImage);
 }
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
 /* Wiring up the Darken/Lighten button */
