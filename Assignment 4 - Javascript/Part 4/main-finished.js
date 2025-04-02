67e647cec3f986a2766b44f6670b89e869bd4453
nxte-nxte <nathan.betton@dcmail.ca>
2025-04-02 12:43:31 -0400

    Collision Detection added

--- Assignment 4 - Javascript/Part 4/main-finished.js
+++ Assignment 4 - Javascript/Part 4/main-finished.js
@@ -124,7 +124,20 @@ class EvilCircle extends Shape {
     }
   }
 
- 
+  collisionDetect() {
+      for (const ball of balls) {
+        if (ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+    
+          if (distance < this.size + ball.size) {
+            ball.exists = false;
+            count--;
+          }
+        }
+      }
+    }
 }
 
 
