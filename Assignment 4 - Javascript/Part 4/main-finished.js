ca96a3ab9bccb068e68ae4ef1d4121e69d34b0af
nxte-nxte <nathan.betton@dcmail.ca>
2025-04-02 12:53:12 -0400

    Added missing BeginPath

--- Assignment 4 - Javascript/Part 4/main-finished.js
+++ Assignment 4 - Javascript/Part 4/main-finished.js
@@ -98,6 +98,7 @@ class EvilCircle extends Shape {
     if (!this.exists) {
       return;
     }
+    ctx.beginPath();
     ctx.lineWidth = 3;
     ctx.strokeStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
